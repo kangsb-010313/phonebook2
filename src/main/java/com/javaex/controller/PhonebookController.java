5b5a0afc7dbe474354d6e83929845968f8e039cf
kangsb-010313 <kangsb668@naver.com>
2025-06-16 17:02:08 +0900

    pb2 250616 17:03

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -3,6 +3,7 @@ package com.javaex.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -37,7 +38,14 @@ public class PhonebookController extends HttpServlet {
 		
 		System.out.println(personList);
 		
-		//html + List
+		//저 밑에 있는 list.jsp 에게 후반일을 시킨다
+		// 1)request객체에 데이터를 넣어준다
+		request.setAttribute("pList", personList);
+		
+		// 2)list.jsp에 request객체와 response객체를 보낸다
+		//*forward 포워드
+		RequestDispatcher rd = request.getRequestDispatcher("/list.jsp");
+		rd.forward(request, response);
 		
 		
 		
